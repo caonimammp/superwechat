c439227c7d528889dfb8cd9597a9708b0fd0d391
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-19 15:05:35 +0800

    添加MFGT类，优化startActivity的写法。

---
+++ SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,30 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.MainActivity;
+import cn.ucai.superwechat.ui.SplashActivity;
+
+/**
+ * Created by Administrator on 2017/5/19.
+ */
+
+public class MFGT {
+
+    public static void gotoLogin(Activity activity) {
+        mfgt(activity,LoginActivity.class);
+    }
+    public static void gotoMain(Activity activity) {
+        mfgt(activity,MainActivity.class);
+    }
+
+    public static void mfgt(Context context,Class clazz) {
+        context.startActivity(new Intent(context,clazz));
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+
+}
