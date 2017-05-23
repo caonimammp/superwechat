1effbdedd554ca785161cd4ff5aa920ddb7369a7
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-23 21:33:47 +0800

    修改昵称，下载头像接口的定义和实现

--- SuperWeChat/src/cn/ucai/superwechat/data/net/IUserModel.java
+++ SuperWeChat/src/cn/ucai/superwechat/data/net/IUserModel.java
@@ -2,6 +2,8 @@ package cn.ucai.superwechat.data.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 
 /**
  * Created by Administrator on 2017/5/19.
@@ -11,4 +13,9 @@ public interface IUserModel {
     void Register(Context context, String userName, String nick, String passWord, OnCompleteListener<String> listener);
     void unRegister(Context context,String userName,OnCompleteListener<String> listener);
     void findUserByUserName(Context context,String userName,OnCompleteListener<String> listener);
+    void updateUserNick(Context context,String username,String nickname,OnCompleteListener<String> listener);
+    void uploadAvatar(Context context, String username, File file, OnCompleteListener<String> listener);
+    void addContact(Context context,String username,String cname, OnCompleteListener<String> listener);
+    void loadContact(Context context,String username,OnCompleteListener<String> listener);
+    void delContact(Context context,String username,String cname, OnCompleteListener<String> listener);
 }
--- SuperWeChat/src/cn/ucai/superwechat/data/net/UserModel.java
+++ SuperWeChat/src/cn/ucai/superwechat/data/net/UserModel.java
@@ -2,6 +2,8 @@ package cn.ucai.superwechat.data.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.easeui.domain.User;
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.utils.OkHttpUtils;
@@ -37,6 +39,58 @@ public class UserModel implements IUserModel{
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_USER)
                 .addParam(I.User.USER_NAME,userName)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void updateUserNick(Context context, String username, String nickname, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nickname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void uploadAvatar(Context context, String username, File file,
+                             OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    @Override
+    public void addContact(Context context, String username,String cname, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_ADD_CONTACT)
+                .addParam(I.Contact.USER_NAME,username)
+                .addParam(I.Contact.CU_NAME,cname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void loadContact(Context context, String username, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void delContact(Context context, String username, String cname, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DELETE_CONTACT)
+                .addParam(I.Contact.USER_NAME,username)
+                .addParam(I.Contact.CU_NAME,cname)
                 .targetClass(String.class)
                 .execute(listener);
     }
