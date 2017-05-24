e047752dfb33acfc05c66cadf7dd6d1f700821f8
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-24 10:04:14 +0800

    EaseBaseActivity中添加TatilBar

--- easeui/src/cn/ucai/easeui/ui/EaseBaseActivity.java
+++ easeui/src/cn/ucai/easeui/ui/EaseBaseActivity.java
@@ -24,10 +24,11 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 import cn.ucai.easeui.controller.EaseUI;
+import cn.ucai.easeui.widget.EaseTitleBar;
 
 @SuppressLint({"NewApi", "Registered"})
 public class EaseBaseActivity extends FragmentActivity {
-
+    protected EaseTitleBar titleBar;
     protected InputMethodManager inputMethodManager;
 
     @Override
@@ -43,10 +44,20 @@ public class EaseBaseActivity extends FragmentActivity {
                 return;
             }
         }
-        
+        titleBar = (EaseTitleBar) findViewById(cn.ucai.easeui.R.id.title_bar);
         inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
     }
-    
+    public void showTitleBar(){
+        if(titleBar != null){
+            titleBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideTitleBar(){
+        if(titleBar != null){
+            titleBar.setVisibility(View.GONE);
+        }
+    }
 
     @Override
     protected void onResume() {
