fd0415952c3821681aa9fe316b320dd03b896389
李鹏 <kkkkkkkkkkkop.qq.com>
2017-05-31 20:46:18 +0800

    添加MainActivity通讯录有消息时的小红点

--- SuperWeChat/src/cn/ucai/superwechat/ui/MainActivity.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -415,16 +415,12 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 	 * update the total unread count
 	 */
 	public void updateUnreadAddressLable() {
-//		runOnUiThread(new Runnable() {
-//			public void run() {
-//				int count = getUnreadAddressCountTotal();
-//				if (count > 0) {
-//					unreadAddressLable.setVisibility(View.VISIBLE);
-//				} else {
-//					unreadAddressLable.setVisibility(View.INVISIBLE);
-//				}
-//			}
-//		});
+		runOnUiThread(new Runnable() {
+			public void run() {
+				int count = getUnreadAddressCountTotal();
+				mLayoutTabHost.setHasNew(1,count>0);
+			}
+		});
 
 	}
 
