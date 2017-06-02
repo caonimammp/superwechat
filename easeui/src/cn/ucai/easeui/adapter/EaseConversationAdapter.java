9d61f23c6eaa1d44f53574e3b0397add3ec5c879
李鹏 <kkkkkkkkkkkop.qq.com>
2017-06-02 20:49:05 +0800

    会话列表显示头像和昵称

--- easeui/src/cn/ucai/easeui/adapter/EaseConversationAdapter.java
+++ easeui/src/cn/ucai/easeui/adapter/EaseConversationAdapter.java
@@ -119,8 +119,8 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
             holder.name.setText(room != null && !TextUtils.isEmpty(room.getName()) ? room.getName() : username);
             holder.motioned.setVisibility(View.GONE);
         }else {
-            EaseUserUtils.setUserAvatar(getContext(), username, holder.avatar);
-            EaseUserUtils.setUserNick(username, holder.name);
+            EaseUserUtils.setAPPUserAvatar(getContext(), username, holder.avatar);
+            EaseUserUtils.setAPPUserNick(username, holder.name);
             holder.motioned.setVisibility(View.GONE);
         }
 
